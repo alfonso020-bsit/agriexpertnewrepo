4078d4b0ce256e362826c1c1e40d74bd10f1649b
ALFONSO_BSIT <alfonsoangelica120@gmail.com>
2025-12-02 16:22:36 +0800

    Update settings.py

--- AgriExpertProject/settings.py
+++ AgriExpertProject/settings.py
@@ -32,12 +32,12 @@ SUPABASE_BUCKET = env("SUPABASE_BUCKET", default="media")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#0=6%o^v_s^vqili0g%k#!v(pwfc@nrhfi3y2!nrt*m!qqe0f+'
+SECRET_KEY = env("SECRET_KEY",default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
