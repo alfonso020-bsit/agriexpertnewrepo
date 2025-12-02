6ed82ccced4c155e1fd1fd41c3fcef0c9b9dddfc
alfonso020-bsit <alfonsoangelica120@gmail.com>
2025-12-02 18:04:16 +0800

    Fix syntax error in settings.py - ALLOWED_HOSTS

--- AgriExpertProject/settings.py
+++ AgriExpertProject/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env("SECRET_KEY",default="")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
